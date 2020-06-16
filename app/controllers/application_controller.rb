8974604aa91c3084151fd117834b01d500c2961e
frankfang <frankfang1990@gmail.com>
2020-06-16 22:28:23 +0800

    极限优化代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: {resource: resource}, status: 200
+    else
+      render json: {errors: resource.errors}, status: 400
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
   def create
-    user = User.new
-    user.email = params[:email]
-    user.password = params[:password]
-    user.password_confirmation = params[:password_confirmation]
-    if user.save
-      render json: {resource: user}, status: 200
-    else
-      render json: {errors: user.errors}, status: 400
-    end
+    render_resource User.create create_params
   end
+
+  def create_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
 end
